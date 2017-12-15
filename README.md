aa4b90e9c98a722d36cdad23b31fa64002479ab0
canastro <ricardocanastro@gmail.com>
2017-12-15 22:16:27 +0000

    :tada: create repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Blog
+My personal blog.
+
+# To do:
+* Fix error 404
+* Use preact
+* Unit tests
+
+# Pending articles
+* React Router Code Split
---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+const windowWidth = window.innerWidth;
+
+exports.shouldUpdateScroll = (args) => {
+    // Scroll position only matters on mobile as on larger screens, we use a
+    // modal.
+    if (windowWidth < 750) {
+        return true;
+    }
+    return false;
+};
